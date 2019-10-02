70c4ae53571510ecfafa9dfa1ef9d3405bbdbaa0
brouzuf <brouzuf@gmail.com>
2019-10-02 17:59:22 +0200

    Create InstallWowfiles.sh

---
+++ InstallWowfiles.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+cores=$(nproc --ignore=1)
+
+cp /mangos/mmap-extractor /wow
+cp /mangos/map-extractor /wow
+cp /mangos/vmap-extractor /wow
+
+cd /wow
+
+./map-extractor
+./vmap-extractor
+./mmap-extractor --threads $cores
+
+echo Moving generated files from /wow to /config/wowfiles
+echo This may take a while.
+
+mkdir /config/wowfiles
+mkdir /config/mangoslogs
+
+mv /wow/mmaps /config/wowfiles
+mv /wow/vmaps /config/wowfiles
+mv /wow/maps /config/wowfiles
+mv /wow/dbc /config/wowfiles
+
+ln -s /config/wowfiles/mmaps /mangos
+ln -s /config/wowfiles/vmaps /mangos
+ln -s /config/wowfiles/maps /mangos
+ln -s /config/wowfiles/dbc /mangos
+
+mkdir /config/wowconfig/
+
+rm -R /wow/Buildings
+
+if [ ! -f /config/wowconfig/mangosd.conf ]; then
+   cp /install/mangosd.conf /config/wowconfig
+fi
+
+if [ ! -f /config/wowconfig/realmd.conf ]; then
+   cp /install/realmd.conf /config/wowconfig
+fi
+
+if [ ! -f /config/wowconfig/console.conf ]; then
+   cp /install/console.conf /config/wowconfig
+fi
+
+echo Script Finished
+echo If you want, you can recreate this container without the /wow mapping / world of warcraft installation directory
+echo The needed files are now present in the /config directory.
+
