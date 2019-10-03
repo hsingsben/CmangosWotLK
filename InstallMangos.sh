1f471851d29c4b81e5166172d855850f5a36e755
brouzuf <brouzuf@gmail.com>
2019-10-03 13:43:39 +0200

    Update InstallMangos.sh

--- InstallMangos.sh
+++ InstallMangos.sh
@@ -6,11 +6,6 @@ cd "/sources/build"
   make
 
 mkdir /mangos
-  cp -a /sources/build/contrib/extractor/ad /mangos
-  cp -a /sources/build/contrib/mmap/MoveMapGen /mangos
-  cp -a /sources/build/contrib/vmap_assembler/vmap_assembler /mangos
-  cp -a /sources/build/contrib/vmap_extractor/vmapextract/vmap_extractor /mangos
-  cp -a /sources/build/contrib/extractor_scripts /mangos
   cp -a /sources/build/src/mangosd/mangosd /mangos
   cp -a /sources/build/src/realmd/realmd /mangos
 
