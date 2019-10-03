27887fbd756638beabe894cb0f5ce06197c4ec3f
brouzuf <brouzuf@gmail.com>
2019-10-03 13:08:52 +0200

    Update InstallDatabases.sh

--- InstallDatabases.sh
+++ InstallDatabases.sh
@@ -1,221 +1,376 @@
 #!/bin/sh
 ##############################################################################
-# This utility assist you in setting up your C-mangos database.                #
-# This is a port of InstallDatabases.bat written by Antz for Windows         #
-#                                                                            #
+# 
+#   Simple helper script to insert clean WoTLK-DB
+#
 ##############################################################################
 
+# need to be changed on each official DB/CORE release
+FULLDB_FILE="WoTLKDB_1_2_13992.sql"
+DB_TITLE="v1.2 'Ymiron'"
+NEXT_MILESTONES="0.19 0.20"
 
-RELEASE="Rel21"
-UPDATES="Rel20_to_BaseRel21_Updates"
-DUMP="NO"
+#internal use
+SCRIPT_FILE="InstallFullDB.sh"
+CONFIG_FILE="InstallFullDB.config"
 
-createcharDB="YES"
-createworldDB="YES"
-createrealmDB="YES"
+# testing only
+ADDITIONAL_PATH=""
 
-loadcharDB="YES"
-loadworldDB="YES"
-loadrealmDB="YES"
-dbType="POPULATED"
+#variables assigned and read from $CONFIG_FILE
+DB_HOST="localhost"
+DB_PORT="3306"
+DATABASE=""
+USERNAME="root"
+PASSWORD=$MYSQL_ROOT_PASSWORD
+MYSQL=""
+CORE_PATH=""
+DEV_UPDATES="NO"
+FORCE_WAIT="YES"
 
-updatecharDB="YES"
-updateworldDB="YES"
-updaterealmDB="YES"
+function create_config {
+# Re(create) config file
+cat >  $CONFIG_FILE << EOF
+####################################################################################################
+# This is the config file for the '$SCRIPT_FILE' script
+#
+# You need to insert
+#   DB_HOST:      Host on which the database resides
+#   DB_PORT:      Port on which the database is running
+#   DATABASE:     Your database
+#   USERNAME:     Your username
+#   PASSWORD:     Your password
+#   CORE_PATH:    Your path to core's directory
+#   MYSQL:        Your mysql command (usually mysql)
+#
+####################################################################################################
 
-addRealmList="YES"
+## Define the host on which the mangos database resides (typically localhost)
+DB_HOST="localhost"
 
-svr="localhost"
-user="root"
-pass=$MYSQL_ROOT_PASSWORD
-port="3306"
-wdb="woltkmangos"
-cdb="woltkcharacters"
-rdb="woltkrealmd"
+## Define the port on which the mangos database is running (typically 3306)
+DB_PORT="3306"
 
-printHelp()
-{
-	printf "Usage: $0 [-s] [-u] [-d] [-h]\n"
-	printf "\t-s: Run this script in silent mode, only prompt for the database information\n"
-	printf "\t-u: Run only the updates of the database\n"
-	printf "\t-d: Dump the database configuration into the home directory of the user\n"
-	printf "\t-h: Display this help\n"
-}
+## Define the database in which you want to add clean WoTLK-DB
+DATABASE="wotlkmangos"
 
-printBanner()
-{
-	clear
-	printf " #     #     #    #   #  ###   ###   ####  \n"
-	printf " ##   ##    # #   ##  # #     #   # #      \n"
-	printf " # # # #   #   #  # # # # ### #   #  ###   \n"
-	printf " #  #  #  ####### #  ## #   # #   #     #  \n"
-	printf " #     #  #     # #   #  ###   ###  ####   \n"
-	printf "\n"
-	printf " Database Setup and World Loader v0.01 	\n"
-	printf " ---------------------------------------------- \n"
-	printf "  Website / Forum / Wiki : https://getmangos.eu \n"
-	printf " ---------------------------------------------- \n"
-	printf "\n"
-	printf "\n"
-}
+## Define your username
+USERNAME="root"
 
-printActivities()
-{
-	printf "\tCharacter Database : \tV - Toggle Create DB (${createcharDB})	\n"
-	printf "\t\t\t\tC - Toggle Create Structure (${loadcharDB}) \n"
-	printf "\t\t\t\tB - Apply Character DB updates (${updatecharDB})\n"
-	printf "\n"
-	printf "\t    World Database : \tE - Toggle Create DB (${createworldDB}) \n"
-	printf "\t\t\t\tW - Toggle Create Structure (${loadworldDB}) \n"	
-	if [ "${loadworldDB}" = "YES" ]; then
-		printf "\t\t\t\tD - Toggle World Type (${dbType})\n"
-	fi
-	printf "\t\t\t\tU - Apply World DB updates (${updateworldDB})\n"
-	printf "\n"
-	printf "\t    Realm Database : \tT - Toggle Create DB (${createrealmDB})\n"
-	printf "\t\t\t\tR - Toggle Create Structure (${loadrealmDB})\n"
-	printf "\t\t\t\tY - Apply Realm DB updates (${updaterealmDB})\n"
-	printf "\t\t\t\tL - Toggle Add RealmList Entry (${addRealmList})\n"	
-	printf "\n"
-	printf "\t\t\t\tN - Next Step\n"
-	printf "\t\t\t\tX - Exit\n"
-}
+## Define your password (It is suggested to restrict read access to this file!)
+PASSWORD="mangos"
 
-createCharDB()
-{
-	printf "Creating Character database ${cdb}\n"
-	mysql -u $user -p$pass -q -s -e "Create database ${cdb}"
-	if [ "${loadcharDB}" = "YES" ]; then
-		loadCharDB
-	fi
-}
+## Define the path to your core's folder
+## Core updates located under sql/updates from this mangos-directory will be added automatically
+CORE_PATH=""
 
-loadCharDB()
-{
-	printf "Loading data into character database ${cdb}\n"
-	mysql -u $user -p$pass -q -s ${cdb} < /database/Character/Setup/characterLoadDB.sql
-}
+## Define your mysql programm if this differs
+MYSQL="mysql"
 
-updateCharDB()
-{
-	printf "Updating data into the character database ${cdb}\n"
-	for file in $(ls /database/Character/Updates/*.sql | tr ' ' '|' | tr '\n' ' ')
-	do
-		file=$(echo ${file} | tr '|' ' ')
-		printf "Applying update ${file}\n"
-		mysql -u $user -p$pass -q -s ${cdb} < ${file}
-	done
-}
+## Define if you want to wait a bit before applying the full database
+FORCE_WAIT="YES"
 
-createWorldDB()
-{
-	printf "Creating World database ${wdb}\n"
-	mysql -u $user -p$pass -q -s -e "Create database ${wdb}"
-	if [ "${loadworldDB}" = "YES" ]; then
-		loadWorldDB
-	fi
-}
+## Define if the 'dev' directory for processing development SQL files needs to be used
+## Set the variable to "YES" to use the dev directory
+DEV_UPDATES="NO"
 
-loadWorldDB()
-{
-	printf "Loading data into world database ${wdb}\n"
-	mysql -u $user -p$pass -q -s ${wdb} < /database/World/Setup/mangosdLoadDB.sql
-	
-	if [ "${dbType}" = "POPULATED" ]; then
-		populateWorldDB
-	fi
-}
-
-populateWorldDB()
-{
-	printf "Importing World database ${wdb}\n"
-	for file in $(ls /database/World/Setup/FullDB/*.sql | tr ' ' '|' | tr '\n' ' ') 
-	do
-		file=$(echo ${file} | tr '|' ' ')
-		printf "Importing file ${file}\n"
-		mysql -u $user -p$pass -q -s ${wdb} < ${file}
-		printf "File ${file} imported\n"
-	done
-}
-
-updateWorldDB()
-{
-	printf "Updating data into the World database ${wdb}\n"
-	for file in $(ls /database/World/Updates/*.sql | tr ' ' '|' | tr '\n' ' ')
-	do
-		file=$(echo ${file} | tr '|' ' ')
-		printf "Applying update ${file}\n"
-		mysql -u $user -p$pass -q -s ${wdb} < ${file}
-	done
-}
-
-createRealmDB()
-{
-	printf "Creating realm database ${rdb}\n"
-	mysql -u $user -p$pass -q -s -e "Create database ${rdb}"
-	if [ "${loadrealmDB}" = "YES" ]; then
-		loadRealmDB
-	fi
-}
-
-loadRealmDB()
-{
-	printf "Loading data into realm database ${rdb}\n"	
-	mysql -u $user -p$pass -q -s ${rdb} < /database/Realm/Setup/realmdLoadDB.sql
-}
-
-updateRealmDB()
-{
-	printf "Updating data into the Realm database ${rdb}\n"
-	for file in $(ls /database/Realm/Updates/*.sql | tr ' ' '|' | tr '\n' ' ')
-	do
-		file=$(echo ${file} | tr '|' ' ')
-		printf "Applying update ${file}\n"
-		mysql -u $user -p$pass -q -s ${wdb} < ${file}
-	done
-}
-
-addRealmList()
-{
-	printf "Adding realm list entries\n"
-	mysql -u $user -p$pass -q -s ${rdb} < /database/Tools/updateRealm.sql
-}
-
-
-
-if [ "${createcharDB}" = "YES" ]; then
-	createCharDB
+# Enjoy using the tool
+if [ ! -f $CONFIG_FILE ]
+then
+  create_config
+  display_help
+  exit 1
 fi
 
-if [ "${createworldDB}" = "YES" ]; then
-	createWorldDB
+. $CONFIG_FILE
+export MYSQL_PWD="$PASSWORD"
+MYSQL_COMMAND="$MYSQL -h$DB_HOST -P$DB_PORT -u$USERNAME $DATABASE"
+
+## Print header
+echo
+echo "Welcome to the WoTLK-DB helper $SCRIPT_FILE"
+echo
+
+if [ "$FORCE_WAIT" != "NO" ]
+then
+  echo "ATTENTION: Your database $DATABASE will be reset to WoTLK-DB!"
+  echo "Please bring your repositories up-to-date!"
+  echo "Press CTRL+C to exit"
+  # show a mini progress bar
+  for i in {1..10}
+  do
+   echo -ne .
+   sleep 1
+  done
+  echo .
 fi
 
-if [ "${createrealmDB}" = "YES" ]; then
-	createRealmDB
+## Full Database
+echo "> Processing WoTLK database $DB_TITLE ..."
+$MYSQL_COMMAND < "${ADDITIONAL_PATH}Full_DB/$FULLDB_FILE"
+if [[ $? != 0 ]]
+then
+  echo "ERROR: cannot apply ${ADDITIONAL_PATH}Full_DB/$FULLDB_FILE"
+  exit 1
+fi
+echo "  $DB_TITLE is applied!"
+echo
+echo
+
+## Updates
+echo "> Processing database updates ..."
+COUNT=0
+for UPDATE in "${ADDITIONAL_PATH}Updates/"[0-9]*.sql
+do
+  if [ -e "$UPDATE" ]
+  then
+    echo "    Appending $UPDATE"
+    $MYSQL_COMMAND < "$UPDATE"
+    if [[ $? != 0 ]]
+    then
+      echo "ERROR: cannot apply $UPDATE"
+      exit 1
+    fi
+    ((COUNT++))
+  fi
+done
+if [ "$COUNT" != 0 ]
+then
+  echo "  $COUNT DB updates applied successfully"
+else
+  echo "  Did not find any new DB update to apply"
+fi
+echo
+echo
+
+## Instances
+echo "> Processing instance files ..."
+COUNT=0
+for INSTANCE in "${ADDITIONAL_PATH}Updates/Instances/"[0-9]*.sql
+do
+  if [ -e "$INSTANCE" ]
+  then
+    echo "    Appending $INSTANCE"
+    $MYSQL_COMMAND < "$INSTANCE"
+    if [[ $? != 0 ]]
+    then
+      echo "ERROR: cannot apply $INSTANCE"
+      exit 1
+    fi
+    ((COUNT++))
+  fi
+done
+if [ "$COUNT" != 0 ]
+then
+  echo "  $COUNT Instance files applied successfully"
+else
+  echo "  Did not find any instance file to apply"
+fi
+echo
+echo
+
+#
+#               Core updates
+#
+
+echo "> Trying to retrieve last core update packaged in database ..."
+LAST_CORE_REV=0
+CORE_REVS="$(grep -r "^.*required_[0-9]*.* DEFAULT NULL" ${ADDITIONAL_PATH}Full_DB/* | sed 's/.*required_\([0-9]*\).*/\1/') "
+CORE_REVS+=$(grep -ri '.*alter table.*required_' ${ADDITIONAL_PATH}Updates/* | sed 's/.*required_\([0-9]*\).*required_\([0-9]*\).*/\1 \2/')
+if [ "$CORE_REVS" != "" ]
+then
+  for rev in $CORE_REVS
+  do
+    if [ "$rev" -gt "$LAST_CORE_REV" ]
+    then
+      LAST_CORE_REV=$rev
+    fi
+  done
 fi
 
-if [ "${updatecharDB}" = "YES" ]; then
-	updateCharDB
+if [ "$LAST_CORE_REV" -eq "0" ]
+then
+  echo "ERROR: cannot get last core revision in DB"
+  exit 1
+else
+  echo "  Found last core revision in DB is $LAST_CORE_REV"
+fi
+echo
+echo
+
+# process future release folders
+if [ "$CORE_PATH" != "" ]
+then
+  if [ ! -e $CORE_PATH ]
+  then
+    echo "Path to core provided, but directory not found! $CORE_PATH"
+    exit 1
+  fi
+  UPD_PROCESSED=0
+  UPD_FOUND=0
+
+  for NEXT_MILESTONE in ${NEXT_MILESTONES};
+  do
+    # A new milestone was released, apply additional updates
+    if [ -e ${CORE_PATH}/sql/updates/${NEXT_MILESTONE}/ ]
+    then
+      echo "> Trying to apply core updates from milestone $NEXT_MILESTONE ..."
+      for f in "${CORE_PATH}/sql/archives/${NEXT_MILESTONE}/"*_mangos_*.sql
+      do
+        CUR_REV=$(basename "$f" | sed 's/^\([0-9]*\).*/\1/')
+        if [ "$CUR_REV" -gt "$LAST_CORE_REV" ]
+        then
+          # found a newer core update file
+          echo "    Appending core update `basename $f` to database $DATABASE"
+          $MYSQL_COMMAND < $f
+          if [[ $? != 0 ]]
+          then
+            echo "ERROR: cannot apply $f"
+            exit 1
+          fi
+          ((UPD_PROCESSED++))
+        else
+          ((UPD_FOUND++))
+        fi
+      done
+    fi
+  done
+
+  # Apply remaining files from main folder
+  echo "> Trying to apply additional core updates from path $CORE_PATH ..."
+  for f in "$CORE_PATH/sql/updates/mangos/"*_mangos_*.sql
+  do
+    CUR_REV=$(basename "$f" | sed 's/^\([0-9]*\).*/\1/')
+    if [ "$CUR_REV" -gt "$LAST_CORE_REV" ]
+    then
+      # found a newer core update file
+      echo "    Appending core update `basename $f` to database $DATABASE"
+      $MYSQL_COMMAND < $f
+      if [[ $? != 0 ]]
+      then
+        echo "ERROR: cannot apply $f"
+        exit 1
+      fi
+      ((UPD_PROCESSED++))
+    else
+      ((UPD_FOUND++))
+    fi
+  done
+  echo "  CORE UPDATE PROCESSED: $UPD_PROCESSED"
+  echo "  CORE UPDATE FOUND BUT ALREADY IN DB: $UPD_FOUND"
+  echo
+  echo
+  
+  # Apply dbc folder
+  echo "> Trying to apply $CORE_PATH/sql/base/dbc/original_data ..."
+  for f in "$CORE_PATH/sql/base/dbc/original_data/"*.sql
+  do
+    echo "    Appending DBC file update `basename $f` to database $DATABASE"
+    $MYSQL_COMMAND < $f
+    if [[ $? != 0 ]]
+    then
+      echo "ERROR: cannot apply $f"
+      exit 1
+    fi
+  done
+  echo "  DBC datas successfully applied"
+  echo
+  echo
+  # Apply dbc changes (specific fixes to known wrong/missing data)
+  echo "> Trying to apply $CORE_PATH/sql/base/dbc/cmangos_fixes ..."
+  for f in "$CORE_PATH/sql/base/dbc/cmangos_fixes/"*.sql
+  do
+    echo "    Appending CMaNGOS DBC file fixes `basename $f` to database $DATABASE"
+    $MYSQL_COMMAND < $f
+    if [[ $? != 0 ]]
+    then
+      echo "ERROR: cannot apply $f"
+      exit 1
+    fi
+  done
+  echo "  DBC changes successfully applied"
+  echo
+  echo
+
+  # Apply scriptdev2.sql
+  echo "> Trying to apply $CORE_PATH/sql/scriptdev2/scriptdev2.sql ..."
+  $MYSQL_COMMAND < $CORE_PATH/sql/scriptdev2/scriptdev2.sql
+  if [[ $? != 0 ]]
+  then
+    echo "ERROR: cannot apply $CORE_PATH/sql/scriptdev2/scriptdev2.sql"
+    exit 1
+  fi
+  echo "  ScriptDev2 successfully applied"
+  echo
+  echo
 fi
 
-if [ "${updateworldDB}" = "YES" ]; then
-	updateWorldDB
+#
+#               ACID Full file
+#
+# Apply acid_wotlk.sql
+echo "> Trying to apply ${ADDITIONAL_PATH}ACID/acid_wotlk.sql ..."
+$MYSQL_COMMAND < ${ADDITIONAL_PATH}ACID/acid_wotlk.sql
+if [[ $? != 0 ]]
+then
+  echo "ERROR: cannot apply ${ADDITIONAL_PATH}ACID/acid_wotlk.sql"
+  exit 1
+fi
+echo "  ACID successfully applied"
+echo
+echo
+
+#
+#    DEVELOPERS UPDATES
+#
+if [ "$DEV_UPDATES" == "YES" ]
+then
+  echo "> Trying to apply development updates ..."
+  for UPDATEFILE in ${ADDITIONAL_PATH}dev/*.sql
+  do
+    if [ -e "$UPDATEFILE" ]
+    then
+        for UPDATE in ${ADDITIONAL_PATH}dev/*.sql
+        do
+            echo "    process update $UPDATE"
+            $MYSQL_COMMAND < $UPDATE
+            [[ $? != 0 ]] && exit 1
+        done
+        echo "  Development updates applied"
+    else
+        echo "  No development update to process"
+    fi
+    break
+  done
+  for UPDATEFILE in ${ADDITIONAL_PATH}dev/*/*.sql
+  do
+    if [ -e "$UPDATEFILE" ]
+    then
+        for UPDATE in ${ADDITIONAL_PATH}dev/*/*.sql
+        do
+            echo "    process update $UPDATE"
+            $MYSQL_COMMAND < $UPDATE
+            [[ $? != 0 ]] && exit 1
+        done
+        echo "  Development subupdates applied"
+    else
+        echo "  No development subupdate to process"
+    fi
+    break
+  done
+  echo
+  echo
 fi
 
-if [ "${updaterealmDB}" = "YES" ]; then
-	updateRealmDB
-fi
-
-if [ "${addRealmList}" = "YES" ]; then
-	addRealmList
-fi
+echo "You have now a clean and recent WoTLK-DB database loaded into $DATABASE"
+echo "Enjoy using WoTLK-DB"
+echo
 
 if [ "${DUMP}" = "YES" ]; then
 	printf "Dumping database information...\n"
-	echo "${svr};${port};${user};${pass};${rdb}" > ~/db.conf
-	echo "${svr};${port};${user};${pass};${wdb}" >> ~/db.conf
-	echo "${svr};${port};${user};${pass};${cdb}" >> ~/db.conf
+	echo "${DB_HOST};${DB_PORT};${USERNAME};${PASSWORD};${rdb}" > ~/db.conf
+	echo "${DB_HOST};${DB_PORT};${USERNAME};${PASSWORD};${wdb}" >> ~/db.conf
+	echo "${DB_HOST};${DB_PORT};${USERNAME};${PASSWORD};${cdb}" >> ~/db.conf
 fi
 
 
